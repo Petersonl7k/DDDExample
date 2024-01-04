ef7731ddfe96beab68ccadd27d9a7ac2eddc37f0
PETERSON PAULO DOS SANTOS TEIXEIRA <peterson.psteixeira@senacsp.edu.br>
2024-01-03 22:12:03 -0300

    Atualizar Codigo

---
+++ Application/Controllers/VeiculoController.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Commands;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculoController : ControllerBase
+    {
+        [HttpPost("CadastrarVeiculo")]
+        public IActionResult PostAsync([FromBody] VeiculoCommand command)
+        {
+            return Ok();
+        }
+        public IActionResult SimularAluguel()
+        {
+            return Ok();
+        }
+        public IActionResult Alugar()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Domain/Entidades/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entidades
+{
+    public class Veiculo
+    {
+        public int VeiculoID { get; set; }
+        public string Placa { get; set; }
+        public int AnoFabricacao { get; set; }
+        public ETipoVeiculo tipoVeiculo { get; set; }
+        public string Estado {  get; set; }
+        public EMontadora montadora { get; set; }
+        public bool Alugado { get; set; } = false;
+    }
+}
